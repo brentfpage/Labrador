99e104d1e5eeca1e58aa14414087b6f6ad397d83
brentfpage <brentfpage@gmail.com>
2026-01-18 16:45:48 -0800

    update

--- Desktop_Interface/androidusbdriver.cpp
+++ Desktop_Interface/androidusbdriver.cpp
@@ -50,13 +50,12 @@ unsigned char androidUsbDriver::usbInit(unsigned long VIDin, unsigned long PIDin
     }
 
     libusb_set_option(ctx, LIBUSB_OPTION_NO_DEVICE_DISCOVERY);
+    libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, 4);
     int error = libusb_init(&ctx);
     if(error){
         qDebug() << "libusb_init FAILED with error" << error;
         return error;
     } else qDebug() << "Libusb context initialised";
-
-    libusb_set_debug(ctx, 3);
 
     qDebug() << "Opening Device!";
     //libusb_device * device_ptr = libusb_get_device2(ctx, usbfs_path);
